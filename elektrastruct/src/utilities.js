918a8abf295bb860dbdcf6d6f471e7e102646c64
unknown <bayahov1@gmail.com>
2018-09-24 17:36:20 +0300

    Add a file., which will contain helper functions. Implement a helper function for base paths.

---
+++ elektrastruct/src/utilities.js
@@ -0,0 +1,23 @@
+/**
+ * @file utilities.js
+ *
+ * Helper functions.
+ * 
+ * @author Bilger Yahov <bayahov1@gmail.com>
+ * @version 1.0.0
+ * @copyright © 2018 Bilger Yahov, all rights reserved.
+ */
+
+'use strict';
+
+module.exports = {
+    MODULES_BASE_PATH: `./app/modules/`,
+    
+    getModulesBasePath: () => {
+        return MODULES_BASE_PATH;
+    },
+    
+    constructSingleModuleBasePath: (moduleName) => {
+        return `${MODULES_BASE_PATH}${moduleName}/`;
+    }
+}
