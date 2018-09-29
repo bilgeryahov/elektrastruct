0e1ddc5aaa3e4d300bea5cf10987b01c8fdd4ced
unknown <bayahov1@gmail.com>
2018-09-29 16:14:31 +0300

    Initialize create-module.test.js.

---
+++ elektrastruct/test/unit/create-module/create-module.test.js
@@ -0,0 +1,11 @@
+/**
+ * @file create-module.test.js
+ *
+ * Unit testing for the module creation functions.
+ * 
+ * @author Bilger Yahov <bayahov1@gmail.com>
+ * @version 1.0.0
+ * @copyright © 2018 Bilger Yahov, all rights reserved.
+ */
+
+ 
